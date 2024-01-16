007810661ba37ce4e5dfe82743984776e345e004
Adam-prog1 <adamkasymbekov@gmail.com>
2024-01-16 23:27:43 +0600

    Revert "мусор"
    This reverts commit f644c82e3a7d3f9df1778cab4f560afb4cfe2e53.

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,118 +1,12 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Management;
-using System.Windows;
-using IWshRuntimeLibrary;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Controls;
-using System.Collections.ObjectModel;
-using System.Runtime.InteropServices;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Interop;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace PC_assistant.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        //Fields
-        private ViewModelBase _currentChildView;
-        private string _caption;
-
-        //Properties
-        public UserAccountModel CurrentUserAccount
-        {
-            get
-            {
-                return _currentUserAccount;
-            }
-
-            set
-            {
-                _currentUserAccount = value;
-                OnPropertyChanged(nameof(CurrentUserAccount));
-            }
-        }
-
-        public ViewModelBase CurrentChildView
-        {
-            get
-            {
-                return _currentChildView;
-            }
-
-            set
-            {
-                _currentChildView = value;
-                OnPropertyChanged(nameof(CurrentChildView));
-            }
-        }
-
-        public string Caption
-        {
-            get
-            {
-                return _caption;
-            }
-
-            set
-            {
-                _caption = value;
-                OnPropertyChanged(nameof(Caption));
-            }
-        }
-
-        public IconChar Icon
-        {
-            get
-            {
-                return _icon;
-            }
-
-            set
-            {
-                _icon = value;
-                OnPropertyChanged(nameof(Icon));
-            }
-        }
-
-        //--> Commands
-        public ICommand ShowHomeViewCommand { get; }
-        public ICommand ShowCustomerViewCommand { get; }
-
-        public MainViewModel()
-        {
-            userRepository = new UserRepository();
-            CurrentUserAccount = new UserAccountModel();
-
-            //Initialize commands
-            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
-            ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
-
-            //Default view
-            ExecuteShowHomeViewCommand(null);
-
-            LoadCurrentUserData();
-        }
-
-        private void ExecuteShowCustomerViewCommand(object obj)
-        {
-            CurrentChildView = new CustomerViewModel();
-            Caption = "Customers";
-            Icon = IconChar.UserGroup;
-        }
-
-        private void ExecuteShowHomeViewCommand(object obj)
-        {
-            CurrentChildView = new HomeViewModel();
-            Caption = "Dashboard";
-            Icon = IconChar.Home;
-        }
     }
+}
