23a190f9b83d41384c96357e78ce6f1aa8ea09ab
Adam-prog1 <adamkasymbekov@gmail.com>
2024-01-16 21:42:34 +0600

    Создал Styles, ViewModel и Views

---
+++ ViewModels/UpdatesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_assistant.ViewModels
+{
+    public class UpdatesViewModel : ViewModelBase 
+    {
+    }
+}
