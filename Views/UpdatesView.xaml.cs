42dad2883e560d2a494f7fc99ab0e6b83f46f2bc
Adam-prog1 <adamkasymbekov@gmail.com>
2024-01-19 14:41:27 +0600

    Перенёс код логики работы вкладки Обновления из MainWindow.xaml.cs в UpdatesView.xaml.cs

--- Views/UpdatesView.xaml.cs
+++ Views/UpdatesView.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
+
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PC_assistant.Views
 {
@@ -23,6 +15,61 @@ namespace PC_assistant.Views
         public UpdatesView()
         {
             InitializeComponent();
+        }
+
+        // Код для отключения обновлений
+        private void DisableWindowsUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Ваш код для отключения обновлений
+            ExecuteCommand("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate\" /v AUOptions /t REG_DWORD /d 1 /f");
+            MessageBox.Show("Windows Update отключено. Перезапустите компьютер для применения изменений.", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        // Код для вкючения обновлений
+        private void EnableWindowsUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Ваш код для включения обновлений
+            ExecuteCommand("reg delete \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate\" /v AUOptions /f");
+            MessageBox.Show("Windows Update включено. Перезапустите компьютер для применения изменений.", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void ExecuteCommand(string command)
+        {
+            try
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    Verb = "runas"  // Запустить с правами администратора
+                };
+
+                Process process = new Process
+                {
+                    StartInfo = processStartInfo
+                };
+
+                process.Start();
+
+                StreamWriter sw = process.StandardInput;
+                StreamReader sr = process.StandardOutput;
+
+                sw.WriteLine(command);
+                sw.WriteLine("exit");
+
+                string output = sr.ReadToEnd();
+
+                process.WaitForExit();
+                process.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
