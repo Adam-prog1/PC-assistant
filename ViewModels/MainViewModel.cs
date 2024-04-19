289a1a7cc7c06600cd6127a03e18af0e721433c5
Адам Касымбек уулу <adamkasymbekov@gmail.com>
2024-04-19 11:41:40 +0300

    Изменил названия главной страницы в разделе контента с Home на Главная

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -66,7 +66,7 @@ namespace PC_assistant.ViewModels
         private void ExecuteShowHomeViewCommand(object obj)
         {
             CurrentChildView = _homeViewModel;
-            Caption = "Home";
+            Caption = "Главная";
             Icon = IconChar.Home;
         }
 
